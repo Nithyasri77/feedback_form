f24be4d60feae234ffdd5b50d90f74d2b60752a5
Nithyasri77 <nithyasri.2006.g@gmail.com>
2026-04-07 11:29:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "feedback_form",  // 👈 ADD THIS LINE
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
